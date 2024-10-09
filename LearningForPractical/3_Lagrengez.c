5b2a7096fc63fb98530f89e0b4f702246cb7d93b
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-10-10 01:17:10 +0530

    Lagrengez

--- LearningForPractical/3_Lagrengez.c
+++ LearningForPractical/3_Lagrengez.c
@@ -1,34 +1,33 @@
-//Lagrange's Interpolation Method
 #include<stdio.h>
-
-int main()
-{
+#include<math.h>
+int main(){
     int n;
-    printf("Enter no. of terms\n");
+    printf("Enter the number of terms you want:");
     scanf("%d",&n);
-    float X[n],Y[n],x,sum=0,term;
-    int i,j;
-   
-    printf("Enter Values of X \n");
-    for(i=0;i<n;i++)
-          scanf("%f",&X[i]);
-    printf("Enter Values of Y\n");
-    for(i=0;i<n;i++)
-          scanf("%f",&Y[i]);
-          
-    printf("Enter value of x for which you want y\n");
+    float X[n],Y[n];
+    printf("Enter the x intervals:\n");
+    for(int i=0;i<n;i++){
+        scanf("%f",&X[i]);
+    }
+     printf("Enter the y intervals:\n");
+    for(int i=0;i<n;i++){
+        scanf("%f",&Y[i]);
+    }
+    float x;
+    printf("Enter the value of for which you want to interpolate y:");
     scanf("%f",&x);
-
-    // Applying the Formula  
-    for(i=0;i<n;i++)
-    {
-        term=1;
-        for(j=0;j<n;j++)
-        {   
-            if(i!=j)
-               term = term * ((x - X[j])/(X[i]-X[j]));
+    float sum=0;
+  
+    for(int i=0;i<n;i++){
+        float term=1;
+        for(int j=0;j<n;j++){
+            if(i!=j){
+                term=term*((x-X[j])/(X[i]-X[j]));
+            }
         }
-        sum=sum + term * Y[i];    
+        sum+=term*Y[i];
     }
     printf("\nValue at X=%g is = %f", x,sum);
+    return 0;
 }
+
--- LearningForPractical/temp.c
+++ LearningForPractical/temp.c
@@ -1,56 +1,34 @@
-#include <stdio.h>
+//Lagrange's Interpolation Method
+#include<stdio.h>
 
-int main() {
+int main()
+{
     int n;
-    printf("Enter the number of terms you want: ");
-    scanf("%d", &n);
+    printf("Enter no. of terms\n");
+    scanf("%d",&n);
+    float X[n],Y[n],x,sum=0,term;
+    int i,j;
+   
+    printf("Enter Values of X \n");
+    for(i=0;i<n;i++)
+          scanf("%f",&X[i]);
+    printf("Enter Values of Y\n");
+    for(i=0;i<n;i++)
+          scanf("%f",&Y[i]);
+          
+    printf("Enter value of x for which you want y\n");
+    scanf("%f",&x);
 
-    // Use fixed-size array or consider using dynamic memory allocation
-    float arr[n][n + 1];
-
-    printf("Enter the values of x:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%f", &arr[i][0]);  // Store x values in the first column
-    }
-
-    printf("Enter the values of y:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%f", &arr[i][1]);  // Store y values in the second column
-    }
-
-    // Constructing the difference table
-    for (int j = 2; j <= n; j++) {
-        for (int i = 0; i <= n - j; i++) {
-            arr[i][j] = arr[i + 1][j - 1] - arr[i][j - 1];
+    // Applying the Formula  
+    for(i=0;i<n;i++)
+    {
+        term=1;
+        for(j=0;j<n;j++)
+        {   
+            if(i!=j)
+               term = term * ((x - X[j])/(X[i]-X[j]));
         }
+        sum=sum + term * Y[i];    
     }
-
-    // Print the difference table
-    printf("Difference Table:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <=n-i; j++) {
-            printf("%f\t", arr[i][j]); // Print each element with tab spacing
-        }
-        printf("\n");
-    }
-
-    float x;
-    printf("Enter the value of x for which you want y: ");
-    scanf("%f", &x);
-    
-    float h = arr[1][0] - arr[0][0];  // Calculate h
-    float u = (x - arr[0][0]) / h;  // Calculate u
-    float y = arr[0][1];  // Start with the first y value
-    float u1 = u;
-    int fact = 1;
-
-    // Calculate the interpolated value of y
-    for (int i = 2; i <= n; i++) {
-        fact *= (i - 1);  // Factorial calculation
-        y += (u1 * arr[0][i]) / fact;  // Adding terms to y
-        u1 *= (u - (i - 1));  // Update u1 for next term
-    }
-
-    printf("\nValue at X = %f is = %f\n", x, y);
-    return 0;
+    printf("\nValue at X=%g is = %f", x,sum);
 }
